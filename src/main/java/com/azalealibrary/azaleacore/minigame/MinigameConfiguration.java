072d588999f32ef774064d02b329b5cceb713bee
Noah Greff <greffnoah@gmail.com>
2022-09-22 19:07:29 +0200

    removed world in `MinigameConfiguration`

--- src/main/java/com/azalealibrary/azaleacore/minigame/MinigameConfiguration.java
+++ src/main/java/com/azalealibrary/azaleacore/minigame/MinigameConfiguration.java
@@ -1,19 +1,16 @@
 package com.azalealibrary.azaleacore.minigame;
 
-import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MinigameConfiguration {
 
     private final JavaPlugin plugin;
-    private final World world;
     private final int tickRate;
     private final int graceTickDuration;
     private final int roundTickDuration;
 
-    private MinigameConfiguration(JavaPlugin plugin, World world, int tickRate, int graceTickDuration, int roundTickDuration) {
+    private MinigameConfiguration(JavaPlugin plugin, int tickRate, int graceTickDuration, int roundTickDuration) {
         this.plugin = plugin;
-        this.world = world;
         this.tickRate = tickRate;
         this.graceTickDuration = graceTickDuration;
         this.roundTickDuration = roundTickDuration;
@@ -21,10 +18,6 @@ public final class MinigameConfiguration {
 
     public JavaPlugin getPlugin() {
         return plugin;
-    }
-
-    public World getWorld() {
-        return world;
     }
 
     public int getTickRate() {
@@ -39,21 +32,19 @@ public final class MinigameConfiguration {
         return roundTickDuration;
     }
 
-    public static Builder create(JavaPlugin plugin, World world) {
-        return new Builder(plugin, world);
+    public static Builder create(JavaPlugin plugin) {
+        return new Builder(plugin);
     }
 
     public static class Builder  {
 
         private final JavaPlugin plugin;
-        private final World world;
         private int tickRate = 1;           // 1hz
         private int graceTickDuration = 10; // 10s
         private int roundTickDuration = 30; // 30s
 
-        private Builder(JavaPlugin plugin, World world) {
+        private Builder(JavaPlugin plugin) {
             this.plugin = plugin;
-            this.world = world;
         }
 
         public Builder tickRate(int rate) {
@@ -72,7 +63,7 @@ public final class MinigameConfiguration {
         }
 
         public MinigameConfiguration build() {
-            return new MinigameConfiguration(plugin, world, tickRate, graceTickDuration, roundTickDuration);
+            return new MinigameConfiguration(plugin, tickRate, graceTickDuration, roundTickDuration);
         }
     }
 }
