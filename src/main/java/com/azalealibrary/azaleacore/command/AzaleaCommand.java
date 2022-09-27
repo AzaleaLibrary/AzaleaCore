9f63618b920ce9c7879ab737af4ab21880326b20
Noah Greff <greffnoah@gmail.com>
2022-09-27 17:49:54 +0200

    updated `PropertyCommand`

--- src/main/java/com/azalealibrary/azaleacore/command/AzaleaCommand.java
+++ src/main/java/com/azalealibrary/azaleacore/command/AzaleaCommand.java
@@ -11,7 +11,6 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 // TODO - currently composition-based, what about inheritance?
--- src/main/java/com/azalealibrary/azaleacore/command/PropertyCommand.java
+++ src/main/java/com/azalealibrary/azaleacore/command/PropertyCommand.java
@@ -3,24 +3,15 @@ package com.azalealibrary.azaleacore.command;
 import com.azalealibrary.azaleacore.api.broadcast.message.ChatMessage;
 import com.azalealibrary.azaleacore.api.broadcast.message.Message;
 import org.bukkit.command.CommandSender;
-import org.bukkit.permissions.PermissionDefault;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.java.annotation.command.Command;
 import org.bukkit.plugin.java.annotation.command.Commands;
-import org.bukkit.plugin.java.annotation.permission.Permission;
 
 import javax.annotation.Nonnull;
 import java.util.Arrays;
 import java.util.List;
 
-@SuppressWarnings("unused")
 @Commands(@Command(name = AzaleaCommand.COMMAND_PREFIX + "property"))
-@Permission(name = "admin", defaultValue = PermissionDefault.OP)
 public class PropertyCommand extends AzaleaCommand {
-
-    public PropertyCommand(JavaPlugin plugin) {
-        super("property", plugin);
-    }
 
     @Override
     protected Message execute(@Nonnull CommandSender sender, String[] params) {
@@ -29,15 +20,6 @@ public class PropertyCommand extends AzaleaCommand {
 
     @Override
     protected List<String> onTabComplete(CommandSender sender, String option, String[] params) {
-//        if (params.length == 0) {
-//            return AzaleaApi.MINIGAMES.values().stream().map(controller -> controller.getMinigame().getName()).toList();
-//        } else {
-//            return List.of();
-//        }
-//        AzaleaApi.getInstance().getMinigames().values().stream()
-//                .filter((c -> c.getCurrentRound().getPlayers().contains(player)))
-//                .findFirst()
-//                .ifPresent(controller -> controller.getCurrentRound().getBroadcaster().broadcast(message));
-        return List.of();
+        return List.of("wer", "ert", "ertrty", "rty", "rtyytui", "tyu", "tyuuyio", "yui", "iuo", "oip");
     }
 }
