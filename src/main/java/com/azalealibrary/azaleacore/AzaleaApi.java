b30e8122df5a10a03d022a21326bbdf5558a0ee6
Noah Greff <greffnoah@gmail.com>
2022-09-27 16:39:40 +0200

    removed unused `AzaleaApi` methods

--- src/main/java/com/azalealibrary/azaleacore/AzaleaApi.java
+++ src/main/java/com/azalealibrary/azaleacore/AzaleaApi.java
@@ -10,35 +10,12 @@ import java.util.*;
 
 public final class AzaleaApi {
 
-    private static final List<Minigame<?>> REGISTERED_MINIGAME = new ArrayList<>();
-    private static final HashMap<World, MinigameController<?, ?>> RUNNING_MINIGAMES = new HashMap<>();
-
-    @SuppressWarnings("unchecked")
-    public static <M extends Minigame<? extends Round<?>>, R extends Round<M>> void registerMinigame(M minigame) {
-        Optional<M> current = (Optional<M>) REGISTERED_MINIGAME.stream().filter(m -> Objects.equals(m.getName(), minigame.getName())).findFirst();
-
-        if (current.isPresent()) {
-            throw new IllegalArgumentException("Minigame with name '" + minigame.getName() + "' already registered.");
-        }
-
-        REGISTERED_MINIGAME.add(minigame);
-    }
-
-    @SuppressWarnings("unchecked")
-    public static <M extends Minigame<? extends Round<M>>, R extends Round<M>> MinigameController<M, R> createController(String name, MinigameConfiguration configuration) {
-        Optional<M> minigame = (Optional<M>) REGISTERED_MINIGAME.stream().filter(m -> Objects.equals(m.getName(), name)).findFirst();
-
-        if (minigame.isEmpty()) {
-            throw new IllegalArgumentException("Minigame with name '" + name + "' not registered.");
-        }
-
-        return createController(minigame.get(), configuration);
-    }
+    public static final HashMap<World, MinigameController<?, ?>> MINIGAMES = new HashMap<>();
 
     public static <M extends Minigame<? extends Round<M>>, R extends Round<M>> MinigameController<M, R>  createController(M minigame, MinigameConfiguration configuration) {
         World world = minigame.getWorld();
         MinigameController<M, R> controller = new MinigameController<>(minigame, configuration);
-        RUNNING_MINIGAMES.put(world, controller);
+        MINIGAMES.put(world, controller);
         return controller;
     }
 }
