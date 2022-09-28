16753c65f13fa58c0c1e589eb4a9246dcb3fa58f
Noah Greff <greffnoah@gmail.com>
2022-09-28 18:15:31 +0200

    fixed `onTabComplete`

--- src/main/java/com/azalealibrary/azaleacore/command/PropertyCommand.java
+++ src/main/java/com/azalealibrary/azaleacore/command/PropertyCommand.java
@@ -3,13 +3,19 @@ package com.azalealibrary.azaleacore.command;
 import com.azalealibrary.azaleacore.AzaleaApi;
 import com.azalealibrary.azaleacore.api.broadcast.message.ChatMessage;
 import com.azalealibrary.azaleacore.api.broadcast.message.Message;
+import com.azalealibrary.azaleacore.api.configuration.MinigameProperty;
+import com.azalealibrary.azaleacore.api.configuration.Property;
+import com.azalealibrary.azaleacore.minigame.MinigameController;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.java.annotation.command.Command;
 import org.bukkit.plugin.java.annotation.command.Commands;
 
 import javax.annotation.Nonnull;
 import java.util.List;
+import java.util.Optional;
 
 @Commands(@Command(name = PropertyCommand.NAME))
 public class PropertyCommand extends AzaleaCommand {
@@ -27,9 +33,34 @@ public class PropertyCommand extends AzaleaCommand {
 
     @Override
     protected List<String> onTabComplete(CommandSender sender, List<String> params) {
-        if (params.size() < 2) {
+        if (params.size() == 1) {
             return AzaleaApi.MINIGAMES.values().stream().map(controller -> controller.getMinigame().getName()).toList();
+        } else {
+            String minigameInput = params.get(0);
+            Optional<MinigameController<?, ?>> controller = AzaleaApi.MINIGAMES.values().stream()
+                    .filter(c -> c.getMinigame().getName().equals(minigameInput))
+                    .findFirst();
+
+            if (controller.isPresent()) {
+                List<MinigameProperty<?>> properties = controller.get().getMinigame().getProperties();
+
+                if (params.size() == 2) {
+                    return properties.stream().map(Property::getName).toList();
+                } else {
+                    String propertyInput = params.get(1);
+                    Optional<MinigameProperty<?>> property = properties.stream()
+                            .filter(p -> p.getName().equals(propertyInput))
+                            .findFirst();
+
+                    if (property.isPresent()) {
+                        return property.get().suggest((Player) sender);
+                    } else {
+                        return List.of(ChatColor.RED + "Could not find '" + propertyInput + "' property.");
+                    }
+                }
+            } else {
+                return List.of(ChatColor.RED + "Could not find '" + minigameInput + "' minigame.");
+            }
         }
-        return List.of("N/A");
     }
 }
