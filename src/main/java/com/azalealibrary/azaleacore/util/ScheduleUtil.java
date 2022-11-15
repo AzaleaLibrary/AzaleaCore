3739a0c6ef7aeb09d7f239b76f54b49d67db96a8
Noah Greff <greffnoah@gmail.com>
2022-11-15 01:07:55 +0100

    updated `ScheduleUtil`

--- src/main/java/com/azalealibrary/azaleacore/util/ScheduleUtil.java
+++ src/main/java/com/azalealibrary/azaleacore/util/ScheduleUtil.java
@@ -6,29 +6,30 @@ import org.bukkit.Bukkit;
 
 public final class ScheduleUtil {
 
-    public static void doWhile(int duration, Runnable onInterval, Runnable onDone) {
-        doWhile(duration, 1, onInterval, onDone);
+    public static int doDelayed(int delay, Runnable onDone) {
+        return Bukkit.getScheduler().scheduleSyncDelayedTask(Main.INSTANCE, onDone, delay);
     }
 
-    public static void doWhile(int duration, int interval, Runnable onInterval) {
-        doWhile(duration, interval, onInterval, Runnables.doNothing());
+    public static int doWhile(int duration, Runnable onInterval, Runnable onDone) {
+        return doWhile(duration, 1, onInterval, onDone);
     }
 
-    public static void doWhile(int duration, int interval, Runnable onInterval, Runnable onDone) {
+    public static int doWhile(int duration, int interval, Runnable onInterval) {
+        return doWhile(duration, interval, onInterval, Runnables.doNothing());
+    }
+
+    public static int doWhile(int duration, int interval, Runnable onInterval, Runnable onDone) {
         int eventId = Bukkit.getScheduler().scheduleSyncRepeatingTask(Main.INSTANCE, onInterval, 0, interval);
         Bukkit.getScheduler().scheduleSyncDelayedTask(Main.INSTANCE, () -> {
             try {
                 onDone.run();
             } catch (Exception exception) {
-                System.err.println("Error occurred while running scheduled task.");
+                System.err.println("Error occurred while running scheduled task: " + exception.getMessage());
                 exception.printStackTrace();
             } finally {
                 Bukkit.getScheduler().cancelTask(eventId);
             }
         }, duration);
-    }
-
-    public static void doDelayed(int delay, Runnable onDone) {
-        Bukkit.getScheduler().scheduleSyncDelayedTask(Main.INSTANCE, onDone, delay);
+        return eventId;
     }
 }
