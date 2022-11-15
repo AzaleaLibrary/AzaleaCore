15beff6c6d3b0009ed098b2ce5cfa46934fa195e
Noah Greff <greffnoah@gmail.com>
2022-11-15 01:28:19 +0100

    fixed issue with round ticker

--- src/main/java/com/azalealibrary/azaleacore/minigame/round/RoundTicker.java
+++ src/main/java/com/azalealibrary/azaleacore/minigame/round/RoundTicker.java
@@ -6,7 +6,6 @@ import com.azalealibrary.azaleacore.room.MinigameRoom;
 import com.azalealibrary.azaleacore.util.ScheduleUtil;
 import org.bukkit.Bukkit;
 
-import java.util.Optional;
 import java.util.function.Consumer;
 
 public class RoundTicker {
@@ -31,45 +30,48 @@ public class RoundTicker {
         this.round = round;
 
         /* SETUP */ round.onSetup(new RoundEvent.Setup(room));
+
         graceEventId = ScheduleUtil.doDelayed(configuration.getGraceTickDuration() * configuration.getTickRate(), () -> {
+
             /* START */ round.onStart(new RoundEvent.Start(room));
             roundEventId = ScheduleUtil.doWhile(configuration.getRoundTickDuration(), configuration.getTickRate(), () -> {
+
                 RoundEvent.Tick tickEvent = new RoundEvent.Tick(room);
                 /* TICK */ round.onTick(tickEvent);
                 round.setTick(round.getTick() + 1);
 
-                /* WIN */ Optional.ofNullable(tickEvent.getCondition())
-                        .ifPresent(w -> handleShouldRestart(round::onWin, new RoundEvent.Win(w, room), true));
-                /* WIN */ room.getMinigame().getWinConditions().stream()
-                        .filter(c -> ((WinCondition<Round>) c).evaluate(round))
-                        .findFirst()
-                        .ifPresent(w -> handleShouldRestart(round::onWin, new RoundEvent.Win(w, room), true));
-            }, /* END */ this::cancel);
+                if (tickEvent.getCondition() != null) {
+                    /* WIN */ handleShouldRestart(round::onWin, new RoundEvent.Win(tickEvent.getCondition(), room), true);
+                } else {
+                    /* WIN */ room.getMinigame().getWinConditions().stream()
+                            .filter(c -> ((WinCondition<Round>) c).evaluate(round))
+                            .findFirst()
+                            .ifPresent(w -> handleShouldRestart(round::onWin, new RoundEvent.Win(w, room), true));
+                }
+            }, () -> { /* END */ if (isRunning()) cancel(); });
         });
     }
 
     public void cancel() {
         handleShouldRestart(round::onEnd, new RoundEvent.End(room), false);
 
-        if (graceEventId != null && Bukkit.getScheduler().isCurrentlyRunning(graceEventId)) {
+        if (graceEventId != null) {
             Bukkit.getScheduler().cancelTask(graceEventId);
             graceEventId = null;
         }
-        if (roundEventId != null && Bukkit.getScheduler().isCurrentlyRunning(roundEventId)) {
+        if (roundEventId != null) {
             Bukkit.getScheduler().cancelTask(roundEventId);
             roundEventId = null;
         }
     }
 
     private <E extends RoundEvent.End> void handleShouldRestart(Consumer<E> dispatcher, E event, boolean doCancel) {
-        if (isRunning()) {
-            dispatcher.accept(event);
+        dispatcher.accept(event);
 
-            if (event.doRestart()) {
-                round.setTick(0);
-            } else if (doCancel) {
-                cancel();
-            }
+        if (event.doRestart()) {
+            round.setTick(0);
+        } else if (doCancel) {
+            cancel();
         }
     }
 }
