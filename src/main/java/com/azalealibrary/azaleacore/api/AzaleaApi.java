b8677da6cb5717db4c1b22b88d7cfeb66e5e14ee
Noah Greff <greffnoah@gmail.com>
2022-11-16 16:56:14 +0100

    added `AzaleaApi`

---
+++ src/main/java/com/azalealibrary/azaleacore/api/AzaleaApi.java
@@ -0,0 +1,43 @@
+package com.azalealibrary.azaleacore.api;
+
+import com.azalealibrary.azaleacore.foundation.AzaleaException;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class AzaleaApi<T> {
+
+    private final Map<String, T> objects = new HashMap<>();
+
+    public ImmutableList<String> getKeys() {
+        return ImmutableList.copyOf(objects.keySet());
+    }
+
+    public ImmutableList<T> getObjects() {
+        return ImmutableList.copyOf(objects.values());
+    }
+
+    public ImmutableMap<String, T> getEntries() {
+        return ImmutableMap.copyOf(objects);
+    }
+
+    public T get(String key) {
+        return objects.get(key);
+    }
+
+    public void add(String key, T object) {
+        if (objects.containsKey(key)) {
+            throw new AzaleaException("Object with key '" + key + "' already exists.");
+        }
+        objects.put(key, object);
+    }
+
+    public void remove(String key) {
+        if (!objects.containsKey(key)) {
+            throw new AzaleaException("Object with key '" + key + "' does not exists.");
+        }
+        objects.remove(key);
+    }
+}
