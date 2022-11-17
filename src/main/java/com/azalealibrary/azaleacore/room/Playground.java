f5dcb4012b10ca831dba9e14a7646998b1e82cd2
Noah Greff <greffnoah@gmail.com>
2022-11-17 15:08:02 +0100

    `Playground` stores template instead of world

--- src/main/java/com/azalealibrary/azaleacore/room/Playground.java
+++ src/main/java/com/azalealibrary/azaleacore/room/Playground.java
@@ -1,26 +1,21 @@
 package com.azalealibrary.azaleacore.room;
 
 import org.bukkit.Location;
-import org.bukkit.World;
 
-import java.util.Arrays;
+import java.io.File;
 import java.util.List;
 
 public class Playground {
 
     private final String name;
-    private final World world;
+    private final File template;
     private final Location spawn;
     private final List<String> tags;
     // TODO - world borders
 
-    public Playground(String name, World world, Location spawn, String... tags) {
-        this(name, world, spawn, Arrays.asList(tags));
-    }
-
-    public Playground(String name, World world, Location spawn, List<String> tags) {
+    public Playground(String name, File template, Location spawn, List<String> tags) {
         this.name = name;
-        this.world = world;
+        this.template = template;
         this.spawn = spawn;
         this.tags = tags;
     }
@@ -29,8 +24,8 @@ public class Playground {
         return name;
     }
 
-    public World getWorld() {
-        return world;
+    public File getTemplate() {
+        return template;
     }
 
     public Location getSpawn() {
