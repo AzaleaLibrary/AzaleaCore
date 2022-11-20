a565162483874ef18c863150443a9e973ffeddd0
Noah Greff <greffnoah@gmail.com>
2022-11-20 07:16:53 +0100

    added `AzaleaCommand` descriptor and configurator

--- src/main/java/com/azalealibrary/azaleacore/command/core/AzaleaCommand.java
+++ src/main/java/com/azalealibrary/azaleacore/command/core/AzaleaCommand.java
@@ -1,22 +1,103 @@
 package com.azalealibrary.azaleacore.command.core;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import com.azalealibrary.azaleacore.AzaleaCore;
+import com.azalealibrary.azaleacore.foundation.AzaleaException;
+import com.azalealibrary.azaleacore.room.broadcast.message.ChatMessage;
+import com.azalealibrary.azaleacore.room.broadcast.message.Message;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandMap;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
 
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
-public @interface AzaleaCommand {
-    String name();
+import javax.annotation.Nonnull;
+import java.lang.reflect.Field;
+import java.util.*;
 
-    String description() default "";
+public abstract class AzaleaCommand extends Command {
 
-    String[] aliases() default {};
+    private final List<ExecutionHandler> executors;
+    private final List<CompletionHandler> completers;
 
-    String permission() default "";
+    protected AzaleaCommand(CommandDescriptor descriptor) {
+        super(descriptor.getName(), descriptor.getDescription(), descriptor.getUsage(), Arrays.asList(descriptor.getAliases()));
+        setPermission(descriptor.getPermission());
+        setPermissionMessage(descriptor.getPermissionMessage());
 
-    String permissionMessage() default "";
+        CommandConfigurator configurator = new CommandConfigurator();
+        configure(configurator);
+        this.executors = configurator.getExecutors();
+        this.completers = configurator.getCompleters();
+    }
 
-    String usage() default "";
+    protected abstract void configure(CommandConfigurator configurator);
+
+    @Override
+    public final boolean execute(@Nonnull CommandSender sender, @Nonnull String label, @Nonnull String[] args) {
+        try {
+            Arguments arguments = new Arguments(this, Arrays.asList(args));
+            Executor executor = executors.stream()
+                    .filter(e -> e.applyWhen(sender, arguments))
+                    .findFirst()
+                    .orElseThrow(() -> new AzaleaException("Invalid Azalea command issued.", getUsage()));
+
+            Message message = executor.execute(sender, arguments);
+            if (message != null) {
+                message.post(AzaleaCore.PLUGIN_ID, sender);
+            }
+        } catch (AzaleaException exception) {
+            ChatMessage.failure(exception.getMessage()).post(AzaleaCore.PLUGIN_ID, sender);
+
+            for (String message : exception.getMessages()) {
+                ChatMessage.failure(message).post(AzaleaCore.PLUGIN_ID, sender);
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            String error = exception.getMessage() != null ? exception.getMessage() : exception.toString();
+            ChatMessage.failure(error).post(AzaleaCore.PLUGIN_ID, sender);
+        }
+        return true;
+    }
+
+    @Override
+    public final @Nonnull List<String> tabComplete(@Nonnull CommandSender sender, @Nonnull String label, @Nonnull String[] args) {
+        Arguments arguments = new Arguments(this, Arrays.asList(args));
+        Optional<CompletionHandler> optional = completers.stream()
+                .filter(c -> c.applyWhen(sender, arguments))
+                .findFirst();
+
+        List<String> output = optional.map(completer -> new ArrayList<>(completer.suggest(sender, arguments)))
+                .orElseGet(ArrayList::new);
+
+        String last = arguments.size() > 0 ? arguments.get(arguments.size() - 1) : "";
+        output.sort(Comparator.<String, Boolean>comparing(s -> s.contains(last)).reversed());
+        return output;
+    }
+
+    public static void register(JavaPlugin plugin, Class<?> clazz) {
+        if (clazz.isAssignableFrom(AzaleaCommand.class) || !clazz.isAnnotationPresent(AzaCommand.class)) {
+            throw new IllegalArgumentException("Registering non-command class as Azalea command.");
+        }
+
+        AzaCommand annotation = clazz.getAnnotation(AzaCommand.class);
+        String name = annotation.name();
+        CommandDescriptor descriptor = new CommandDescriptor(name,
+                annotation.description(),
+                annotation.usage(),
+                annotation.aliases(),
+                annotation.permission(),
+                annotation.permissionMessage()
+        );
+
+        try {
+            Field field = Bukkit.getServer().getClass().getDeclaredField("commandMap");
+            field.setAccessible(true);
+
+            CommandMap map = (CommandMap) field.get(Bukkit.getServer());
+            AzaleaCommand command = (AzaleaCommand) clazz.getConstructor(CommandDescriptor.class).newInstance(descriptor);
+            map.register(plugin.getName(), command);
+        } catch (Exception exception) {
+            throw new RuntimeException("An error occurred while register Azalea command.", exception);
+        }
+    }
 }
---
+++ src/main/java/com/azalealibrary/azaleacore/command/core/CommandConfigurator.java
@@ -0,0 +1,60 @@
+package com.azalealibrary.azaleacore.command.core;
+
+import com.azalealibrary.azaleacore.room.broadcast.message.Message;
+import org.bukkit.command.CommandSender;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommandConfigurator {
+
+    private final List<ExecutionHandler> executors = new ArrayList<>();
+    private final List<CompletionHandler> completers = new ArrayList<>();
+
+    public List<ExecutionHandler> getExecutors() {
+        return executors;
+    }
+
+    public List<CompletionHandler> getCompleters() {
+        return completers;
+    }
+
+    public CommandConfigurator executeWhen(Condition conditional, Executor executor) {
+        return addExecutor(new ExecutionHandler() {
+            @Override
+            public boolean applyWhen(CommandSender sender, Arguments arguments) {
+                return conditional.applyWhen(sender, arguments);
+            }
+
+            @Override
+            public @Nullable Message execute(CommandSender sender, Arguments arguments) {
+                return executor.execute(sender, arguments);
+            }
+        });
+    }
+
+    public CommandConfigurator completeWhen(Condition conditional, Completer completer) {
+        return addCompleter(new CompletionHandler() {
+            @Override
+            public boolean applyWhen(CommandSender sender, Arguments arguments) {
+                return conditional.applyWhen(sender, arguments);
+            }
+
+            @Override
+            public List<String> suggest(CommandSender sender, Arguments arguments) {
+                return completer.suggest(sender, arguments);
+            }
+        });
+    }
+
+    public CommandConfigurator addExecutor(ExecutionHandler executor) {
+        this.executors.add(executor);
+        return this;
+    }
+
+    public CommandConfigurator addCompleter(CompletionHandler completer) {
+        this.completers.add(completer);
+        return this;
+    }
+}
