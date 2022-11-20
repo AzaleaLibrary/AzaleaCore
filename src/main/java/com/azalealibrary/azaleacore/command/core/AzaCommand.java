066a8dc4c8a2c4385424fd361169ae1f1e1d5aca
Noah Greff <greffnoah@gmail.com>
2022-11-20 07:16:24 +0100

    added `AzaCommand`

---
+++ src/main/java/com/azalealibrary/azaleacore/command/core/AzaCommand.java
@@ -0,0 +1,22 @@
+package com.azalealibrary.azaleacore.command.core;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface AzaCommand {
+    String name();
+
+    String description() default "";
+
+    String[] aliases() default {};
+
+    String permission() default "";
+
+    String permissionMessage() default "";
+
+    String usage() default "";
+}
