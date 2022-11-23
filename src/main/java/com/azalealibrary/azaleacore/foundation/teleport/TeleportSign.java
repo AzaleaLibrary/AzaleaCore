fd27acb4a7f755e7abf160b062325a8403da2d3a
Noah Greff <greffnoah@gmail.com>
2022-11-23 07:45:01 +0100

    added `TeleportSign`

---
+++ src/main/java/com/azalealibrary/azaleacore/foundation/teleport/TeleportSign.java
@@ -0,0 +1,50 @@
+package com.azalealibrary.azaleacore.foundation.teleport;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
+
+import java.util.Objects;
+import java.util.function.Consumer;
+
+public abstract class TeleportSign {
+
+    private final Location signLocation;
+    private final Location teleportLocation;
+
+    public TeleportSign(Location signLocation, Location teleportLocation) {
+        this.signLocation = signLocation;
+        this.teleportLocation = teleportLocation;
+    }
+
+    public Location getTeleportLocation() {
+        return teleportLocation;
+    }
+
+    public Location getSignLocation() {
+        return signLocation;
+    }
+
+    public void teleport(Player player) {
+        player.teleport(teleportLocation);
+    }
+
+    public void update() {
+        update(this::paint);
+    }
+
+    public void update(Consumer<Sign> decorator) {
+        World world = Objects.requireNonNull(signLocation.getWorld());
+        BlockState state = world.getBlockState(signLocation).getBlock().getState();
+
+        if (state instanceof Sign sign) {
+            decorator.accept(sign);
+            sign.setEditable(false);
+            sign.update();
+        }
+    }
+
+    protected abstract void paint(Sign sign);
+}
