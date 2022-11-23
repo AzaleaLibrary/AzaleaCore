3d4c42f085a5d46ced805f12c0a6ba991e0e14ca
Noah Greff <greffnoah@gmail.com>
2022-11-23 07:45:19 +0100

    added `LobbyTeleportSign`

---
+++ src/main/java/com/azalealibrary/azaleacore/foundation/teleport/LobbyTeleportSign.java
@@ -0,0 +1,25 @@
+package com.azalealibrary.azaleacore.foundation.teleport;
+
+import com.azalealibrary.azaleacore.AzaleaCore;
+import org.bukkit.ChatColor;
+import org.bukkit.DyeColor;
+import org.bukkit.Location;
+import org.bukkit.block.Sign;
+
+public class LobbyTeleportSign extends TeleportSign {
+
+    public LobbyTeleportSign(Location signLocation) {
+        super(signLocation, AzaleaCore.getLobby().getSpawnLocation()); // TODO - review
+        update(); // update lobby sign only on creation
+    }
+
+    @Override
+    protected void paint(Sign sign) {
+        sign.setGlowingText(true);
+        sign.setColor(DyeColor.GREEN);
+        sign.setLine(0, ChatColor.YELLOW + "EXIT" + ChatColor.GREEN + " _o       ");
+        sign.setLine(1, ChatColor.GREEN + "/ /\\_ ");
+        sign.setLine(2, ChatColor.GREEN + "     _/\\  " + ChatColor.YELLOW + "LOBBY ");
+        sign.setLine(3, ChatColor.GREEN + "/  ");
+    }
+}
