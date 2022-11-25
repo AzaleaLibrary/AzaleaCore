2607fa2700b7baf410e6422ee9d2529e4e1cd5c6
Noah Greff <greffnoah@gmail.com>
2022-11-25 07:52:38 +0100

    added `AssignmentPolicy`

---
+++ src/main/java/com/azalealibrary/azaleacore/foundation/configuration/property/AssignmentPolicy.java
@@ -0,0 +1,38 @@
+package com.azalealibrary.azaleacore.foundation.configuration.property;
+
+import org.bukkit.ChatColor;
+
+import java.util.function.Predicate;
+
+public final class AssignmentPolicy<T> {
+
+    public static final AssignmentPolicy<Integer> POSITIVE_INTEGER = AssignmentPolicy.create(value -> value > 0, "Number must be positive.");
+
+    private final String message;
+    private final Predicate<T> predicate;
+
+    private AssignmentPolicy(String message, Predicate<T> predicate) {
+        this.message = message;
+        this.predicate = predicate;
+    }
+
+    public String getMessage(T value) {
+        return String.format(ChatColor.RED + message, value);
+    }
+
+    public boolean canAssign(T value) {
+        return predicate.test(value);
+    }
+
+    public static <T> AssignmentPolicy<T> anything() {
+        return create(value -> true);
+    }
+
+    public static <T> AssignmentPolicy<T> create(Predicate<T> predicate) {
+        return create(predicate, "Can not assign property with value '%s'.");
+    }
+
+    public static <T> AssignmentPolicy<T> create(Predicate<T> predicate, String message) {
+        return new AssignmentPolicy<>(message, predicate);
+    }
+}
