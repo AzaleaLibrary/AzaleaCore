67b43cff62f9347b9e4fbe388698d7d8c5fc58d7
Noah Greff <greffnoah@gmail.com>
2022-11-25 16:44:14 +0100

    removed unused classes

--- src/main/java/com/azalealibrary/azaleacore/foundation/configuration/property/primitive/PrimitiveTypeCompleter.java
+++
@@ -1,5 +0,0 @@
-package com.azalealibrary.azaleacore.foundation.configuration.property.primitive;
-
-public interface PrimitiveTypeCompleter<T> {
-    String suggest(T currentValue);
-}
--- src/main/java/com/azalealibrary/azaleacore/foundation/configuration/property/primitive/PrimitiveTypeParser.java
+++
@@ -1,5 +0,0 @@
-package com.azalealibrary.azaleacore.foundation.configuration.property.primitive;
-
-public interface PrimitiveTypeParser<T> {
-    T parseValue(String input);
-}
