5911b76113c6a7e60dc525c2bab5667cf55c3d4f
Noah Greff <greffnoah@gmail.com>
2022-11-30 16:18:49 +0100

    added `Configurable` getValue/setValue

--- src/main/java/com/azalealibrary/azaleacore/foundation/configuration/Configurable.java
+++ src/main/java/com/azalealibrary/azaleacore/foundation/configuration/Configurable.java
@@ -1,26 +1,33 @@
 package com.azalealibrary.azaleacore.foundation.configuration;
 
+import com.azalealibrary.azaleacore.foundation.AzaleaException;
 import com.azalealibrary.azaleacore.foundation.configuration.property.ConfigurableProperty;
 import com.azalealibrary.azaleacore.foundation.serialization.Serializable;
 import org.bukkit.configuration.ConfigurationSection;
 
 import javax.annotation.Nonnull;
 import java.util.List;
-import java.util.Objects;
 
 public interface Configurable extends Serializable {
 
     List<ConfigurableProperty<?>> getProperties();
 
-    default <T> void updateProperty(ConfigurableProperty<T> property, T value) {
-        for (ConfigurableProperty<?> configurableProperty : getProperties()) {
-            if (Objects.equals(configurableProperty.getName(), property.getName())) {
-                if (configurableProperty.get().getClass() == property.get().getClass()) {
-                    // FORGE this.clazz.equals(property.clazz) && this.name.equals(property.name);
-                    ((ConfigurableProperty<T>) configurableProperty).set(value); // TODO - review
-                }
-            }
-        }
+    default <T> T getValue(ConfigurableProperty<T> property) {
+        ConfigurableProperty<?> found = getProperties().stream()
+                .filter(p -> p.equals(property))
+                .findFirst()
+                .orElseThrow(() -> new AzaleaException("Could not find property '" + property.getName() + "'."));
+
+        return (T) found.get();
+    }
+
+    default <T> void setValue(ConfigurableProperty<T> property, T value) {
+        ConfigurableProperty<?> found = getProperties().stream()
+                .filter(p -> p.equals(property))
+                .findFirst()
+                .orElseThrow(() -> new AzaleaException("Could not find property '" + property.getName() + "'."));
+
+        ((ConfigurableProperty<T>) found).set(value);
     }
 
     @Override
