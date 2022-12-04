79daaeabc0a881c3c278ce6c25a77d865912815e
Noah Greff <greffnoah@gmail.com>
2022-12-04 22:46:02 +0100

    added `Manager` classes

---
+++ src/main/java/com/azalealibrary/azaleacore/manager/PartyManager.java
@@ -0,0 +1,59 @@
+package com.azalealibrary.azaleacore.manager;
+
+import com.azalealibrary.azaleacore.foundation.message.ChatMessage;
+import com.azalealibrary.azaleacore.party.Party;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.Objects;
+
+public class PartyManager extends Manager<Party> {
+
+    private static final PartyManager MANAGER = new PartyManager();
+
+    public static PartyManager getInstance() {
+        return MANAGER;
+    }
+
+    private PartyManager() {
+        super("party");
+    }
+
+    public @Nullable Party get(Player player) {
+        return getAll().stream().filter(p -> p.isHere(player)).findFirst().orElse(null);
+    }
+
+    public Party create(String name, Player owner) {
+        Party party = new Party(name);
+        party.getConfiguration().setPartyOwner(owner);
+        return party;
+    }
+
+    public void delete(Party party) {
+        party.broadcast(ChatMessage.important("Party has been disbanded."));
+
+        for (Player player : party.getPlayers()) {
+            party.removePlayer(player, false);
+        }
+        remove(party);
+    }
+
+    @Override
+    protected void serializeEntry(ConfigurationSection section, Party party) {
+        section.set("name", party.getName());
+        section.set("players", party.getPlayers());
+        party.getConfiguration().serialize(section.createSection("configs"));
+    }
+
+    @Override
+    protected Party deserializeEntry(ConfigurationSection section) {
+        String name = section.getString("name");
+        Party party = new Party(name);
+        party.getConfiguration().deserialize(Objects.requireNonNull(section.getConfigurationSection("configs")));
+        List<Player> players = Objects.requireNonNull((List<Player>) section.getList("players"));
+        players.forEach(player -> party.addPlayer(player, false));
+        return party;
+    }
+}
---
+++ src/main/java/com/azalealibrary/azaleacore/manager/PlaygroundManager.java
@@ -0,0 +1,89 @@
+package com.azalealibrary.azaleacore.manager;
+
+import com.azalealibrary.azaleacore.foundation.AzaleaConfiguration;
+import com.azalealibrary.azaleacore.foundation.AzaleaException;
+import com.azalealibrary.azaleacore.foundation.message.ChatMessage;
+import com.azalealibrary.azaleacore.foundation.registry.MinigameIdentifier;
+import com.azalealibrary.azaleacore.minigame.Minigame;
+import com.azalealibrary.azaleacore.playground.Playground;
+import com.azalealibrary.azaleacore.util.FileUtil;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.WorldCreator;
+import org.bukkit.configuration.ConfigurationSection;
+
+import javax.annotation.Nonnull;
+import java.io.File;
+import java.util.Objects;
+
+public class PlaygroundManager extends Manager<Playground> {
+
+    private static final PlaygroundManager MANAGER = new PlaygroundManager();
+
+    public static PlaygroundManager getInstance() {
+        return MANAGER;
+    }
+
+    private PlaygroundManager() {
+        super("playground");
+    }
+
+    public Playground create(String name, MinigameIdentifier identifier, File map) {
+        File playgroundDir = FileUtil.getPlayground(name);
+
+        if (exists(name)) {
+            throw new AzaleaException("Playground '" + name + "' already exists.");
+        }
+
+        FileUtil.copyDirectory(map, playgroundDir);
+        FileUtil.delete(new File(playgroundDir, "uid.dat")); // otherwise may spigot complains
+
+        WorldCreator creator = new WorldCreator(playgroundDir.getName());
+        World world = Bukkit.createWorld(creator);
+
+        Minigame minigame = Minigame.create(identifier);
+        Playground playground = new Playground(name, world, minigame);
+        add(name, playground);
+        return playground;
+    }
+
+    public void delete(Playground playground) {
+        playground.stop(ChatMessage.important("Deleting playground."));
+        remove(playground);
+        FileUtil.delete(FileUtil.getPlayground(playground.getName()));
+    }
+
+    @Override
+    public void deserialize(@Nonnull ConfigurationSection configuration) {
+        super.deserialize(configuration);
+
+        for (File file : FileUtil.getPlaygrounds()) {
+            if (getKeys().stream().noneMatch(key -> key.equals(file.getName()))) {
+                System.out.println("Found stray room '" + file.getName() + "'.");
+
+                if (AzaleaConfiguration.getInstance().shouldRemoveStrayRooms()) {
+                    System.out.println("Removing room '" + file.getName() + "'.");
+                    FileUtil.delete(file);
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void serializeEntry(ConfigurationSection section, Playground playground) {
+        section.set("name", playground.getName());
+        section.set("world", playground.getWorld().getName());
+        section.set("minigame", playground.getMinigame().getIdentifier().toString());
+        playground.getMinigame().serialize(section.createSection("configs"));
+    }
+
+    @Override
+    protected Playground deserializeEntry(ConfigurationSection section) {
+        String name = Objects.requireNonNull(section.getString("name"));
+        World world = Bukkit.getWorld(Objects.requireNonNull(section.getString("world")));
+        String identifier = Objects.requireNonNull(section.getString("minigame"));
+        Minigame minigame = Minigame.create(new MinigameIdentifier(identifier));
+        minigame.deserialize(Objects.requireNonNull(section.getConfigurationSection("configs")));
+        return new Playground(name, world, minigame);
+    }
+}
