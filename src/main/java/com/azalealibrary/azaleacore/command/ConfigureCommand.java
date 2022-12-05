cea6817c0a0629d5ea8e1823f78e179a7bcfab1c
Noah Greff <greffnoah@gmail.com>
2022-12-05 23:44:13 +0100

    added `ConfigureCommand`

--- src/main/java/com/azalealibrary/azaleacore/command/ConfigureCommand.java
+++ src/main/java/com/azalealibrary/azaleacore/command/ConfigureCommand.java
@@ -1,55 +1,68 @@
 package com.azalealibrary.azaleacore.command;
 
-import com.azalealibrary.azaleacore.command.core.AzaCommand;
-import com.azalealibrary.azaleacore.command.core.AzaleaCommand;
-import com.azalealibrary.azaleacore.command.core.CommandConfigurator;
-import com.azalealibrary.azaleacore.command.core.CommandDescriptor;
+import com.azalealibrary.azaleacore.AzaleaCore;
+import com.azalealibrary.azaleacore.foundation.configuration.Configurable;
+import com.azalealibrary.azaleacore.foundation.configuration.property.ConfigurableProperty;
+import com.azalealibrary.azaleacore.foundation.message.ChatMessage;
+import com.azalealibrary.azaleacore.util.TextUtil;
+import org.bukkit.command.CommandSender;
 
-@AzaCommand(name = "!configure")
-public class ConfigureCommand extends AzaleaCommand {
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
 
-    private static final String ROOM = "getPlayground";
-    private static final String MINIGAME = "minigame";
+public abstract class ConfigureCommand extends CommandNode {
 
     private static final String SET = "set";
     private static final String RESET = "reset";
     private static final String INFO = "info";
 
-    public ConfigureCommand(CommandDescriptor descriptor) {
-        super(descriptor);
+    public ConfigureCommand() {
+        super("configure");
     }
 
     @Override
-    protected void configure(CommandConfigurator configurator) {
-//        configurator.completeWhen((sender, arguments) -> arguments.size() == 1, (sender, arguments) -> AzaleaRoomApi.getInstance().getKeys());
-//        configurator.completeWhen((sender, arguments) -> arguments.size() == 2, (sender, arguments) -> List.of(ROOM, MINIGAME));
-//        configurator.completeWhen((sender, arguments) -> arguments.size() == 3, (sender, arguments) -> getProperties(arguments).stream().map(ConfigurableProperty::getName).toList());
-//        configurator.completeWhen((sender, arguments) -> arguments.size() == 4, (sender, arguments) -> List.of(SET, RESET, INFO));
-//        configurator.completeWhen((sender, arguments) -> arguments.is(3, SET), (sender, arguments) -> {
-//            Optional<ConfigurableProperty<?>> property = getProperties(arguments).stream().filter(p -> arguments.is(2, p.getName())).findFirst();
-//            return property.isPresent() ? property.get().suggest(sender, arguments.subArguments(4)) : List.of();
-//        });
-//        configurator.executeWhen((sender, arguments) -> arguments.size() > 3, this::execute);
+    public List<String> complete(CommandSender sender, Arguments arguments) {
+        if (arguments.size() == 1) {
+            return completeConfigurable(sender, arguments);
+        } else {
+            Configurable configurable = arguments.find(0, "configurable", this::getConfigurable);
+            List<ConfigurableProperty<?>> properties = configurable.getProperties();
+
+            if (arguments.size() == 2) {
+                return properties.stream().map(ConfigurableProperty::getName).toList();
+            } else if (arguments.size() == 3) {
+                return List.of(SET, RESET, INFO);
+            } else if (arguments.size() > 3 && arguments.is(2, SET)) {
+                return properties.stream()
+                        .filter(p -> p.getName().equals(arguments.get(1)))
+                        .findFirst().map(p -> p.suggest(sender, arguments.subArguments(3)))
+                        .orElse(List.of());
+            }
+        }
+        return new ArrayList<>();
     }
 
-//    private Message execute(CommandSender sender, Arguments arguments) {
-//        ConfigurableProperty<?> property = arguments.find(2, "property", input -> getProperties(arguments).stream().filter(p -> p.getName().equals(input)).findFirst().orElse(null));
-//        String action = arguments.matchesAny(3, "action", SET, RESET, INFO);
-//
-//        if (action.equals(SET)) {
-//            property.fromCommand(sender, arguments.subArguments(4));
-//            return ChatMessage.info("Property '" + property.getName() + "' updated.");
-//        } else if (action.equals(RESET)) {
-//            property.reset();
-//            return ChatMessage.info("Property '" + property.getName() + "' has been reset.");
-//        } else {
-//            return ChatMessage.info(property.toString());
-//        }
-//    }
-//
-//    private static List<ConfigurableProperty<?>> getProperties(Arguments arguments) {
-//        Room getPlayground = arguments.find(0, "getPlayground", AzaleaRoomApi.getInstance()::get);
-//        String action = arguments.matchesAny(1, "action", ROOM, MINIGAME);
-//        return action.equals(ROOM) ? getPlayground.getConfiguration().getProperties() : getPlayground.getMinigame().getProperties();
-//    }
+    @Override
+    public void execute(CommandSender sender, Arguments arguments) {
+        Configurable configurable = arguments.find(0, "configurable", this::getConfigurable);
+        ConfigurableProperty<?> property = arguments.find(1, "property", input -> configurable.getProperties().stream().filter(p -> p.getName().equals(input)).findFirst().orElse(null));
+        String action = arguments.matchesAny(2, "action", SET, RESET, INFO);
+
+        switch (action) {
+            case SET -> {
+                property.fromCommand(sender, arguments.subArguments(3));
+                ChatMessage.info("Property " + TextUtil.getName(property) + " updated.").post(AzaleaCore.PLUGIN_ID, sender);
+            }
+            case RESET -> {
+                property.reset();
+                ChatMessage.info("Property " + TextUtil.getName(property) + " has been reset.").post(AzaleaCore.PLUGIN_ID, sender);
+            }
+            case INFO -> ChatMessage.info(TextUtil.getString(property)).post(AzaleaCore.PLUGIN_ID, sender);
+        }
+    }
+
+    protected abstract List<String> completeConfigurable(CommandSender sender, Arguments arguments);
+
+    protected abstract @Nullable Configurable getConfigurable(String input);
 }
