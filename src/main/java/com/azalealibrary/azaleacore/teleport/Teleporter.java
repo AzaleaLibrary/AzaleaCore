22261aeecc2f91f3b16a70e57e0ff640c99e3745
Noah Greff <greffnoah@gmail.com>
2022-12-08 04:39:41 +0100

    added `Teleporter`

---
+++ src/main/java/com/azalealibrary/azaleacore/teleport/Teleporter.java
@@ -0,0 +1,24 @@
+package com.azalealibrary.azaleacore.teleport;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+
+public interface Teleporter {
+
+    String getName();
+
+    Type getType();
+
+    Location getPosition();
+
+    Location getTo();
+
+    default void teleport(Player player) {
+        player.teleport(getTo());
+    }
+
+    enum Type {
+        SIGN, STRUCTURE, PORTAL
+    }
+}
