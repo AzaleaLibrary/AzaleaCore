498180c0f0af4c6766c5a205d50feb8d2dde9821
Noah Greff <greffnoah@gmail.com>
2023-01-25 03:59:40 +0100

    updated examples

--- src/main/java/com/azalealibrary/azaleacore/example/winconditions/NoBluePlayers.java
+++ src/main/java/com/azalealibrary/azaleacore/example/winconditions/NoBluePlayers.java
@@ -1,16 +1,17 @@
 package com.azalealibrary.azaleacore.example.winconditions;
 
 import com.azalealibrary.azaleacore.api.WinCondition;
+import com.azalealibrary.azaleacore.example.Registry;
 import com.azalealibrary.azaleacore.round.Round;
 
 public class NoBluePlayers extends WinCondition {
 
     public NoBluePlayers() {
-        super(null, "All blue players have been eliminated", 100);
+        super("All blue players have been eliminated", 100, Registry.RED_TEAM);
     }
 
     @Override
     public boolean evaluate(Round round) {
-        return round.getTeams().getAllInTeam(null).isEmpty();
+        return round.getTeams().getAllInTeam(Registry.BLUE_TEAM).isEmpty();
     }
 }
--- src/main/java/com/azalealibrary/azaleacore/example/winconditions/NoRedPlayers.java
+++ src/main/java/com/azalealibrary/azaleacore/example/winconditions/NoRedPlayers.java
@@ -1,16 +1,17 @@
 package com.azalealibrary.azaleacore.example.winconditions;
 
 import com.azalealibrary.azaleacore.api.WinCondition;
+import com.azalealibrary.azaleacore.example.Registry;
 import com.azalealibrary.azaleacore.round.Round;
 
 public class NoRedPlayers extends WinCondition {
 
     public NoRedPlayers() {
-        super(null, "All red players have been eliminated", 100);
+        super("All red players have been eliminated", 100, Registry.BLUE_TEAM);
     }
 
     @Override
     public boolean evaluate(Round round) {
-        return round.getTeams().getAllInTeam(null).isEmpty();
+        return round.getTeams().getAllInTeam(Registry.RED_TEAM).isEmpty();
     }
 }
