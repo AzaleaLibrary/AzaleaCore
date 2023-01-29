0e082dec46ed0aac38cc5469b198450d7f3dc9c3
Noah <greffnoah@gmail.com>
2023-01-29 17:55:06 +0100

    removed redundant method param

--- src/main/java/com/azalealibrary/azaleacore/util/SerializationUtil.java
+++ src/main/java/com/azalealibrary/azaleacore/util/SerializationUtil.java
@@ -9,34 +9,30 @@ import java.io.File;
 public final class SerializationUtil {
 
     public static void load(String name, final JavaPlugin plugin, final Serializable serializable) {
-        load(name, serializable, FileUtil.insureExists(new File(plugin.getDataFolder(), name + ".yml")));
+        load(serializable, FileUtil.insureExists(new File(plugin.getDataFolder(), name + ".yml")));
     }
 
-    public static void load(String name, final Serializable serializable, final File file) {
-        System.out.println("Loading '" + name + "' data.");
+    public static void load(final Serializable serializable, final File file) {
         YamlConfiguration config = YamlConfiguration.loadConfiguration(file);
 
         try {
             serializable.deserialize(config);
         } catch (Exception exception) {
-            System.out.println("Could not load '" + name + "' data: " + exception);
             exception.printStackTrace();
         }
     }
 
     public static void save(String name, final JavaPlugin plugin, final Serializable serializable) {
-        save(name, serializable, FileUtil.insureExists(new File(plugin.getDataFolder(), name + ".yml")));
+        save(serializable, FileUtil.insureExists(new File(plugin.getDataFolder(), name + ".yml")));
     }
 
-    public static void save(String name, final Serializable serializable, final File file) {
-        System.out.println("Saving '" + name + "' data.");
+    public static void save(final Serializable serializable, final File file) {
         YamlConfiguration config = new YamlConfiguration();
         serializable.serialize(config);
 
         try {
             config.save(file);
         } catch (Exception exception) {
-            System.out.println("Could not save '" + name + "' data: " + exception);
             exception.printStackTrace();
         }
     }
